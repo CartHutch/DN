598d17a5bc0f0e04a035694e2b4bbd48c3348f92
CartHutch <154272974+CartHutch@users.noreply.github.com>
2023-12-20 20:30:35 -0500

    Update CourseSectionGroup.java

--- CourseSectionGroup.java
+++ CourseSectionGroup.java
@@ -3,4 +3,47 @@ import java.util.ArrayList;
 public class CourseSectionGroup
 {
 	private ArrayList<CourseSection> section = new ArrayList<CourseSection>();
+	
+	public void add(CourseSection c)
+	{
+		section.add(c);
+	}
+	
+	public void remove(CourseSection c)
+	{
+		section.remove(c);
+	}
+	
+	public void remove(int index) 
+	{
+		if(index >= 0 && index > section.size())
+		{
+			section.remove(index);
+		}
+		else
+		{
+			System.out.println("invalid index");
+		}
+	}
+	
+	public CourseSection getCourseSection(CourseSection c)
+	{
+		return c;
+	}
+	
+	public int getIndex(int i)
+	{
+		return i;
+	}
+	
+	public int size()
+	{
+		section.size();
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "sections" + section;
+	}
 }
